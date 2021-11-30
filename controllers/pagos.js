188a2e761b084b64bf09ac04811e67ac5fc96029
david cuspoca <cuspoca3d@gmail.com>
2021-11-30 10:31:16 -0600

    Solucion validacion en postusuarios
    error en if(dbCredito) no estaba especificando el .saldo_total

--- controllers/pagos.js
+++ controllers/pagos.js
@@ -8,23 +8,36 @@ const Credito = require('../models/credito');
 const Cliente = require('../models/cliente');
 
 const postPago = async(req=request, res=response) => {
- try {
+  
+  const {credito, pago} = req.body;
+  const fecha = moment().utc(Date);
+  const data = {
+    credito,
+    pago,
+    fecha
+  }
 
-   const {credito, pago} = req.body;
-   const fecha = moment().utc(Date);
-   const data = {
-     credito,
-     pago,
-     fecha
-   }
+  try {
 
-    const nPago = new Pago(data);
-    await nPago.save();
-
-    //  actualizacion del credito
-    // 
+    // verificamos si el pago es menor a lo que debe el cliente
     const dbCredito = await Credito.findById(credito);
-    
+
+    if(pago <= dbCredito.saldo_total){
+
+      // solo si el pago es igual o mayor a lo que el cliente debe se guarda el pago
+      const nPago = new Pago(data);
+      await nPago.save();
+
+    } else if(pago > dbCredito.saldo_total){
+
+      // si el saldo total el menor al pago entonces mandamos un mensaje de error advirtiendo que el el saldo es menor a al pago, y asi la persona sepa cuanto es lo que tiene que poner
+      return res.status(400).json({
+        msg: `Al cliente solo le restan ${dbCredito.saldo_total}`
+      })
+
+    }
+
+    //  actualizacion del credito      
     // extraemos los abonos y el saldo que tenemos en el credito
     const abonos = dbCredito.abonos + pago;
     const saldo_total = dbCredito.saldo_total - pago;
@@ -33,25 +46,24 @@ const postPago = async(req=request, res=response) => {
 
     // si el credito se cancela cambiamos el estado del credito y el estado del cliente significa esto que el credito ha finalizado
     if(uCredito.saldo_total === 0){
-      await Credito.findByIdAndUpdate(credito, {estado: false});
+      await Credito.findByIdAndDelete(credito);
       await Cliente.findByIdAndUpdate(dbCredito.cliente, {estado: false});
     }
-    
-  res.json({
-    ok: true,
-    nPago
-  })
+      
+    res.json({
+      ok: true
+    })
 
- } catch (error) {
-   console.log(error);
-   res.status(500).json({
-     msg: 'Hable con el administrador'
-   })
- }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: 'Hable con el administrador'
+    })
+  }
 };
 
 const getPagos = async(req=request, res = response) => {
-  const {id} = req.query;
+  const { id}  = req.query;
   try{
 
     const pagos = await Pago.find({credito: id});
